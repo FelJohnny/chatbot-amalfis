7974165b897c7759e33448246eb5f3af7ea9e2d7
Felipe Johnny <felipijohnny@outlook.com>
2025-02-03 11:08:14 -0300

    aplicando ajustes

--- src/controller/chatBot_Controller.js
+++ src/controller/chatBot_Controller.js
@@ -89,13 +89,13 @@ class ChatBot_Controller {
                 }
 
                 // 4. Registra a mensagem recebida do cliente
-                await chatbot_services.registraMensagem(
-                  sessao.id,
-                  cliente.retorno.id,
-                  null, // resposta_id é nulo porque é mensagem do cliente
-                  messageBody
-                );
-                console.log("Mensagem do cliente registrada com sucesso");
+                // await chatbot_services.registraMensagem(
+                //   sessao.id,
+                //   cliente.retorno.id,
+                //   null, // resposta_id é nulo porque é mensagem do cliente
+                //   messageBody
+                // );
+                // console.log("Mensagem do cliente registrada com sucesso");
 
                 // 5. Recupera a última mensagem enviada pelo chatbot
                 const ultimaMensagem =
